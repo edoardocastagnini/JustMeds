1498bb2325ddb6fc9230de37b72df58a5462f55b
carlo <carlo.zamuner@gmail.com>
2024-04-22 14:01:10 +0200

    creazione fil js

---
+++ Site/script/generale.js
@@ -0,0 +1,5 @@
+function showRegistrationForm() {
+    document.getElementById("registrationForm").style.display = "block";
+    document.getElementById("loginform").style.display = "none";
+    document.getElementById("registerpopup").style.display = "none";
+}
