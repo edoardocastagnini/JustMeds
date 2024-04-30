cc83ba84da4cc2d627396215d9b269984113fec3
Edoardo <edoardo1604@gmail.com>
2024-04-30 12:09:30 +0200

    Update Ordine.js

--- src/public/models/Ordine.js
+++ src/public/models/Ordine.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const ordineSchema = new mongoose.Schema({
+    utenteID: String,
+    farmaciaID: String,
+    riderID: String,
     
 
-}, { timestamps: true });
+}, { timestamps: true, versionKey: false});
 
 const Ordine = mongoose.model('Ordine', ordineSchema, 'ordini');
 
