c0e90708a2d4b787f2d9d6447225412a88733226
carlo <carlo.zamuner@gmail.com>
2024-05-05 18:04:42 +0200

    fix ridirezioni login
    In base al tipo di login viene dato il permesso e il divieto per l accesso alle pagine

--- src/index.js
+++ src/index.js
@@ -1,93 +1,160 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+const express = require("express");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 const app = express();
-require('dotenv').config();    // console.log(process.env.SUPER_SECRET);
+require("dotenv").config(); // console.log(process.env.SUPER_SECRET);
 // Middleware per il parsing del corpo delle richieste
 app.use(express.json()); // Per supportare il corpo delle richieste in formato JSON
 app.use(express.urlencoded({ extended: true })); // Per supportare il corpo delle richieste URL-encoded
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: "sanremo",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, httpOnly: true, maxAge: 3600000 }, // Assicurati che 'secure' sia true se sei su HTTPS
+  })
+);
 
+const cors = require("cors");
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
-const tokenChecker = require('./public/middlewares/tokenChecker');
+const tokenChecker = require("./public/middlewares/tokenChecker");
 
-const drugRoutes = require('./public/order/farmaci'); 
-app.use('/api', drugRoutes);
+const drugRoutes = require("./public/order/farmaci");
+app.use("/api", drugRoutes);
 
-
-const User = require('./public/models/User'); 
-
-
+const User = require("./public/models/User");
 
 // La tua stringa di connessione Atlas
-const DbURI = 'mongodb+srv://adminuser:adminuser@justmedsdata.d6avjw7.mongodb.net/Data?retryWrites=true&w=majority&appName=JustMedsData';
+const DbURI =
+  "mongodb+srv://adminuser:adminuser@justmedsdata.d6avjw7.mongodb.net/Data?retryWrites=true&w=majority&appName=JustMedsData";
 
-mongoose.connect(DbURI)
-    .then(() => console.log('MongoDB Atlas connected'))
-    .catch((err) => console.error('Error connecting to MongoDB Atlas:', err));
+mongoose
+  .connect(DbURI)
+  .then(() => console.log("MongoDB Atlas connected"))
+  .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
 
-app.set('view engine', 'ejs');
-app.use(morgan('dev'));
-
-
-
+app.set("view engine", "ejs");
+app.use(morgan("dev"));
 
 // Ora usa User per interagire con la collezione users
-app.post('/sign_up', async (req, res) => {
-    try {
-        const user = new User(req.body);
-        await user.save();
-        console.log("Record inserted successfully");
-        setTimeout(() => {
-            res.redirect('login.html');
-        }, 2000);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send(err.message);
-    }
+app.post("/sign_up", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    console.log("Record inserted successfully");
+    setTimeout(() => {
+      res.redirect("login.html");
+    }, 2000);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
 });
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 app.post('/login', async (req, res) => {
-    console.log("Received login request", req.body);
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email: email });
-        console.log("User found:", user);
-        
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        if (password !== user.password) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-        const payload = { id: user._id, email: user.email };
-        const token = jwt.sign(payload, process.env.SUPER_SECRET, { expiresIn: '1h' });
-        console.log("Token:", token);
-        res.json({ success: true, token: 'Bearer ' + token });
-    } catch (err) {
-        console.error("Login error:", err);
-        res.status(500).send(err.message);
-    }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  
+  if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+  }
+
+  if (password !== user.password) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+  req.session.user = { id: user._id, email: user.email, type: user.type };
+  res.json({ success: true, message: 'Logged in successfully', role: user.type });
 });
 
 
+function isAuthenticated(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.status(401).send({ success: false, message: "Not authenticated" });
+  }
+}
 
+app.get("/protected-route", isAuthenticated, (req, res) => {
+  res.send("Access to protected route!");
+});
 
+app.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        return res.status(500).send("Failed to log out.");
+      } else {
+        return res.redirect("/"); // or your login page
+      }
+    });
+  } else {
+    res.end(); // if not logged in, just end the response
+  }
+});
 
-
+app.get("/api/check-login", (req, res) => {
+  if (req.session && req.session.user) {
+    res.json({ isLoggedIn: true, userRole: req.session.user.type });
+    console.log("Ruolo:", req.session.user.type);
+  } else {
+    res.json({ isLoggedIn: false });
+  }
+});
 
 app.get("/", (req, res) => {
-    try {
-        res.redirect('index.html');
-    } catch (error) {
-        console.error('Error handling root route:', error);
-        res.status(500).send('Internal Server Error');
-    }
+  try {
+    res.redirect("index.html");
+  } catch (error) {
+    console.error("Error handling root route:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
+/// Middleware per verificare il ruolo utente
+function checkUserRole(roles) {
+  return (req, res, next) => {
+    const userRole = req.session.user && req.session.user.role; // Assicurati che il ruolo sia memorizzato nella sessione
+    if (roles.includes(userRole)) {
+      next();
+    } else {
+      res
+        .status(403)
+        .send(
+          "Accesso negato: non hai i permessi per accedere a questa pagina"
+        );
+    }
+  };
+}
+
+// Proteggi le rotte con il middleware
+app.get(
+  "/delivery/delivery.html",
+  checkUserRole(["rider", "admin"]),
+  (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "delivery.html"));
+  }
+);
+
+app.get(
+  "/order/order.html",
+  checkUserRole(["ricevente", "admin"]),
+  (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "order.html"));
+  }
+);
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
--- src/public/order/farmaci.js
+++ src/public/order/farmaci.js
@@ -1,38 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('../../index.js'); 
-const Drug = require('../models/Drug.js');
-const tokenChecker = require('../middlewares/tokenChecker');
-const jwt = require('jsonwebtoken');
+const mongoose = require("../../index.js");
+const Drug = require("../models/Drug.js");
+const tokenChecker = require("../middlewares/tokenChecker");
+const jwt = require("jsonwebtoken");
 
+function sessionChecker(req, res, next) {
+  if (req.session && req.session.user) {
+    console.log("Session active for user:", req.session.user);
+    next();
+  } else {
+    console.log("Session not active, blocking request");
+    res.status(403).json({ message: "Access Forbidden: No session present" });
+  }
+}
 
-// Endpoint protetto per ottenere i primi 10 farmaci
-router.get('/drugs', tokenChecker, async (req, res) => {
-    try {
-        const drugs = await Drug.aggregate([{ $sample: { size: 10 } }]).sort({ NomeFarmaco: 1 });
-        console.log("Drugs fetched:", drugs);
-        res.json(drugs);
-    } catch (error) {
-        console.error('Error loading the drugs:', error);
-        res.status(500).json({ message: error.message });
-    }
+router.get("/drugs", async (req, res) => {
+  try {
+    const drugs = await Drug.aggregate([{ $sample: { size: 10 } }]);
+    const isLoggedIn = req.session && req.session.user ? true : false;
+    res.json({ drugs, isLoggedIn });
+  } catch (error) {
+    console.error("Error loading the drugs:", error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Endpoint protetto per la ricerca di farmaci
-router.get('/drugs/search', tokenChecker, async (req, res) => {
-    const searchTerm = req.query.farmaco;
-    try {
-        const drugs = await Drug.find({
-            $or: [
-                { PrincipioAttivo: { $regex: `^${searchTerm}`, $options: 'i' } },
-                { Farmaco: { $regex: `^${searchTerm}`, $options: 'i' } }
-            ]
-        }).sort({ Farmaco: 1 }).limit(20);
-        res.json(drugs);
-    } catch (error) {
-        console.error('Error loading the drugs:', error);
-        res.status(500).json({ message: error.message });
-    }
+// Questa route non dovrebbe richiedere l'autenticazione dell'utente per permettere la ricerca a tutti.
+router.get("/drugs/search", async (req, res) => {
+  const searchTerm = req.query.farmaco;
+  try {
+    const drugs = await Drug.find({
+      $or: [
+        { PrincipioAttivo: { $regex: `^${searchTerm}`, $options: "i" } },
+        { Farmaco: { $regex: `^${searchTerm}`, $options: "i" } },
+      ],
+    }).limit(20);
+    const isLoggedIn = req.session && req.session.user ? true : false;
+    res.json({ drugs, isLoggedIn });
+  } catch (error) {
+    console.error("Error loading the drugs:", error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
