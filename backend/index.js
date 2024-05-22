ea316ab9213827f8458e0c9af65f76bef55badd2
Edoardo <edoardo1604@gmail.com>
2024-05-22 14:37:19 +0200

    split index in client

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,9 @@ app.use("/api", drugRoutes);
 const contattaciRouter = require("./form_request/contattaci");
 app.use('/api', contattaciRouter);
 
+const clientRouter = require("./client_account/client");
+app.use('/api',clientRouter);
+
 const User = require("./models/User");
 const Carrello = require("./models/Carrello");
 
@@ -406,72 +409,6 @@ app.get('/api/farmacie', async (req, res) => {
       res.status(500).json
   }
 });
-
-
-app.get('/api/profile', isAuthenticated, async (req, res) => {
-  const userId = req.session.user.id;
-  try {
-      const user = await User.findById(userId);
-      if (!user) {
-          return res.status(404).json({ message: 'Utente non trovato' });
-      }
-      res.json({
-          name: user.nome,
-          surname: user.cognome,
-          email: user.email,
-          address: user.via,
-          cap: user.cap,
-          province: user.provincia,
-          city: user.città,
-      });
-  } catch (error) {
-      console.error('Errore nel recuperare i dati utente:', error);
-      res.status(500).json({ message: 'Errore interno del server', error });
-  }
-});
-
-app.get('/api/ordini', isAuthenticated, async (req, res) => {
-  const userId = req.session.user.id;
-  try {
-    const ordini = await Ordine.find({ utenteID: userId })
-      .populate('prodotti._id', 'Farmaco');
-
-    const ordiniConFarmacie = await Promise.all(ordini.map(async (ordine) => {
-      const farmacia = await ListaFarmacie.findById(ordine.farmaciaID).select('FARMACIA');
-      return {
-        ...ordine.toObject(),
-        farmaciaNome: farmacia ? farmacia.FARMACIA : 'N/A'
-      };
-    }));
-
-    res.json(ordiniConFarmacie);
-  } catch (error) {
-    console.error('Errore nel recuperare gli ordini:', error);
-    res.status(500).json({ message: 'Errore interno del server', error });
-  }
-});
-
-app.put('/api/editprofile', isAuthenticated, async (req, res) => {
-  const userId = req.session.user.id;
-  const { city, cap, province,address } = req.body;
-  try {
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: 'Utente non trovato' });
-    }
-    user.città = city;
-    user.cap = cap;
-    user.provincia = province;
-    user.via = address;
-    await user.save();
-    res.json({ message: 'Profilo aggiornato con successo' });
-  } catch (error) {
-    console.error('Errore durante l\'aggiornamento del profilo:', error);
-    res.status(500).json({ message: 'Errore interno del server', error });
-  }
-});
-
-
 
 
 
