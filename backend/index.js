4b40e3fb06023555a43a1b9d18fe01c849b05cb1
gvazzolerunitn <gianluigi.vazzoler@studenti.unitn.it>
2024-06-03 15:28:44 +0200

    fix per visualizzazione ordine attuale

--- backend/index.js
+++ backend/index.js
@@ -307,6 +307,22 @@ app.get("/api/orders", isAuthenticated, async (req, res) => {
   }
 });
 
+// VISUALIZZAZIONE ORDINE SPECIFICO
+// Endpoint per recuperare un singolo ordine
+app.get("/api/orders/:orderId", isAuthenticated, async (req, res) => {
+  try {
+    const orderId = req.params.orderId;
+    const order = await Ordine.findById(orderId).populate('prodotti._id');
+    if (!order) {
+      return res.status(404).json({ success: false, message: "Ordine non trovato" });
+    }
+    res.json(order);
+  } catch (error) {
+    console.error("Errore nel recuperare l'ordine:", error);
+    res.status(500).json({ success: false, message: "Errore durante il recupero dell'ordine" });
+  }
+});
+
 // Endpoint per aggiornare lo stato dell'ordine
 app.post('/api/orders/:id/accept', async (req, res) => {
   const orderId = req.params.id;
