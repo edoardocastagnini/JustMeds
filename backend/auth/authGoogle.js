4009fc1dcde2b593a6431869c08beb1ff9b95071
Edoardo <edoardo1604@gmail.com>
2024-06-07 15:06:44 +0200

    imported swagger.yaml

--- backend/auth/authGoogle.js
+++ backend/auth/authGoogle.js
@@ -6,7 +6,7 @@ require("dotenv").config(); // Assicurati di caricare il file .env
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/api/google/callback"
+  callbackURL: "https://justmeds.onrender.com/api/google/callback"
 },
 async function(accessToken, refreshToken, profile, done) {
   console.log("Google Strategy invoked");
